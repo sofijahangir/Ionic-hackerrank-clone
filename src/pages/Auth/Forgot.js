0ab8dda1499036505fda99c6631ac0dee9c4cf80
yingyangcode <marcel.anis.21@gmail.com>
2020-05-16 22:08:46 -0400

    Authentication Pages

---
+++ src/pages/Auth/Forgot.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import NavHeader from '../../components/Header/NavHeader';
+import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton } from '@ionic/react';
+
+const Forgot = () => {
+    return (
+        <IonPage>
+            <NavHeader title="Password Reset" />
+            <IonContent>
+                <IonItem lines="full">
+                    <IonLabel position="floating">Email</IonLabel>
+                    <IonInput name="email" type="text" required></IonInput>
+                </IonItem>
+                <IonRow>
+                    <IonCol>
+                        <IonButton
+                        type="submit"
+                        color="primary"
+                        expand="block"
+                        >
+                            Get Reset Link
+                        </IonButton>
+                    </IonCol>
+                </IonRow>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Forgot;
