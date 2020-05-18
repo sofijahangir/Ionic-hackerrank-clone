05fc7b699a674ed1c0fb03b5056b7df3ff670d30
yingyangcode <marcel.anis.21@gmail.com>
2020-05-18 15:34:21 -0400

    Link Pages and Link List

--- src/pages/Tabs/News.js
+++ src/pages/Tabs/News.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
 import SmallHeader from '../../components/Header/SmallHeader'
 import LargeHeader from '../../components/Header/LargeHeader'
-const News = () => {
-    return (
-        <IonPage>
-            <SmallHeader title="Newsy"/>
-            <IonContent fullscreen>
-                <LargeHeader title="Newsy"/>
-            </IonContent>
-        </IonPage>
-    );
+import LinkList from '../../components/Link/LinkList';
+
+const News = (props) => {
+  return (
+    <IonPage>
+        <SmallHeader title="Newsy"/>
+        <IonContent fullscreen>
+          <LargeHeader title="Newsy"/>
+          <LinkList location={props.location} />
+        </IonContent>
+    </IonPage>
+  );
 };
 
 export default News;
--- src/pages/Tabs/Trending.js
+++ src/pages/Tabs/Trending.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
 import SmallHeader from '../../components/Header/SmallHeader';
 import LargeHeader from '../../components/Header/LargeHeader';
-const Trending = () => {
-    return (
-        <IonPage>
-            <SmallHeader title="Trending"/>
-            <IonContent fullscreen>
-                <LargeHeader title="Trending"/>
-            </IonContent>
-        </IonPage>
-    );
+import LinkList from '../../components/Link/LinkList';
+
+const Trending = (props) => {
+  return (
+    <IonPage>
+      <SmallHeader title="Trending"/>
+      <IonContent fullscreen>
+        <LargeHeader title="Trending"/>
+        <LinkList location={props.location} />
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Trending;
