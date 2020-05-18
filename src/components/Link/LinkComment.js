dbc5e7981117d20f7ec71b72cb9a713d68f17b8f
yingyangcode <marcel.anis.21@gmail.com>
2020-05-18 17:03:01 -0400

    Link Upvotes

---
+++ src/components/Link/LinkComment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LinkComment = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LinkComment;
--- src/pages/Link.js
+++ src/pages/Link.js
@@ -15,7 +15,7 @@ import { Plugins } from "@capacitor/core";
 import UserContext from "../contexts/UserContext";
 import NavHeader from "../components/Header/NavHeader";
 import LinkItem from "../components/Link/LinkItem";
-
+import LinkComment from "../components/Link/LinkComment";
 const { Browser } = Plugins;
 
 const Link = (props) => {
@@ -33,6 +33,27 @@ const Link = (props) => {
     linkRef.get().then((doc) => {
       setLink({ ...doc.data(), id: doc.id });
     });
+  }
+
+  function handleAddVote() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then((doc) => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          const voteCount = updatedVotes.length;
+          linkRef.update({ votes: updatedVotes, voteCount });
+          setLink((prevState) => ({
+            ...prevState,
+            votes: updatedVotes,
+            voteCount: voteCount,
+          }));
+        }
+      });
+    }
   }
 
   function handleDeleteLink() {
@@ -72,9 +93,20 @@ const Link = (props) => {
               <IonRow>
                 <IonCol class="ion-text-center">
                   <LinkItem link={link} browser={openBrowser} />
+                  <IonButton onClick={() => handleAddVote()} size="small">
+                    Upvote
+                  </IonButton>
                 </IonCol>
               </IonRow>
             </IonGrid>
+            {link.comments.map((comment, index) => (
+              <LinkComment
+                key={index}
+                comment={comment}
+                link={link}
+                setLink={setLink}
+              />
+            ))}
           </>
         )}
       </IonContent>
